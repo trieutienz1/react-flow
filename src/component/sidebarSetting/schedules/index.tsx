eb0ba423e932bf9e41b9606e62a1e18c231e7aa5
trieutienz1 <tientd.dzt@gmail.com>
2025-10-13 21:58:22 +0700

    update code

--- src/component/sidebarSetting/schedules/index.tsx
+++ src/component/sidebarSetting/schedules/index.tsx
@@ -1,27 +1,109 @@
-import { Form, Select } from "antd";
-import React, { useState } from "react";
+import { Col, Form, Input, Row, Select, Tabs } from "antd";
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { updateNode } from "../../reducer/flow";
 
 const SchedulesToolbarForm = ({ selectedNode }) => {
   console.log("SchedulesToolbarForm:", selectedNode);
 
+  const { id, data } = selectedNode;
+
   const [mode, setMode] = useState<string>();
+  const dispatch = useDispatch();
+
+  const [unit, setUnit] = useState<string>();
   const [form] = Form.useForm();
 
-  function handleFinish() {}
+  // console.log("form:setUnit", form.getFieldsValue());
+
+  useEffect(() => {
+    console.log("form.getFieldsValue():", form.getFieldsValue());
+  }, [form.getFieldsValue()]);
+
+  const handleValuesChange = (changedValues, allValues) => {
+    console.log("Trường vừa đổi:", changedValues);
+    console.log("Toàn bộ giá trị form:", allValues);
+
+    const payload = {
+      id: id,
+      data: {
+        ...data,
+        ...allValues,
+      },
+    };
+    dispatch(updateNode(payload));
+  };
+
+  const items = [
+    {
+      key: "1",
+      label: "Time to run",
+      children: (
+        <div>
+          <div>adsdkskla</div>
+          <div>adsdkskla</div>
+        </div>
+      ),
+    },
+    {
+      key: "2",
+      label: "Delay",
+      children: (
+        <div>
+          <p>Settings</p>
+
+          <Row gutter={[5, 5]}>
+            <Col xs={16} xl={16}>
+              <Form.Item
+                label="Set time delay"
+                name="timeDelay"
+                rules={[{ required: true }]}
+              >
+                <Input type="number"></Input>
+              </Form.Item>
+            </Col>
+            <Col xs={8} xl={8}>
+              <Form.Item
+                name="unit"
+                label={<span style={{ opacity: 0 }}>unit</span>}
+              >
+                <Select
+                  options={[
+                    { label: "Days", value: "days" },
+                    { label: "Hours", value: "hours" },
+                    { label: "Minutes", value: "minutes" },
+                  ]}
+                ></Select>
+              </Form.Item>
+            </Col>
+          </Row>
+        </div>
+      ),
+    },
+  ];
+
+  const onChange = (key: string) => {
+    console.log(key);
+  };
 
   return (
     <div>
-      <Form form={form} layout="vertical" onFinish={handleFinish}>
-        <Form.Item label="Mode" rules={[{ required: true }]} name="mode">
+      <Form form={form} onValuesChange={handleValuesChange} layout="vertical">
+        <Form.Item
+          label="Timezone"
+          rules={[{ required: true }]}
+          name="timeZone"
+        >
           <Select
             options={[
-              { label: "SET TIME", value: "SET TIME" },
-              { label: "WAITING", value: "WAITING" },
+              { label: "America/Los_Angeles", value: "US" },
+              { label: "Asia/Ho_Chi_Minh", value: "VN" },
             ]}
-            onChange={setMode}
           ></Select>
         </Form.Item>
-        <Form.Item label="Date"></Form.Item>
+        <Form.Item label="">
+          <Tabs defaultActiveKey="1" items={items} onChange={onChange} />
+        </Form.Item>
       </Form>
     </div>
   );
